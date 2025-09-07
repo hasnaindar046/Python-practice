1f1d5a871a8771b9035c6ba37ff2cfda76d339c7
Hasnain dar <hasnaindar1234@gmail.com>
2025-09-07 15:52:30 +0500

    class 1 and class 4

---
+++ class1.py
@@ -0,0 +1,8 @@
+print("enter your marks:")
+marks = int(input())
+if marks <= 50:
+ print("you are passed with A grade")
+elif marks <= 35:
+ print("you are passed with B grade")
+else:
+ print("you are fail")
