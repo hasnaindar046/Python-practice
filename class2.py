9693fd6fb21f91b6f0ca8c30d790d5f8c9785342
Hasnain dar <hasnaindar1234@gmail.com>
2025-09-07 16:15:32 +0500

    update

--- class2.py
+++ class2.py
@@ -1,5 +1,21 @@
 #variables
-name = "hasnain"
-age = 20
-print(name)
-print(age)
+# name = "hasnain"
+# age = 20
+# print(name)
+# print(age)
+
+#data types
+#1- string
+print("hi how are you?")
+# 2- Integer
+print(10)
+# 3- float
+print(1.9)
+# 4- boolean
+print(True)
+# 5- dictionary
+# 6- list
+# 7- tuple
+# 8- set
+
+
