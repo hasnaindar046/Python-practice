2050d27e13394faf0f932f0e575e753d50dc196d
Hasnain dar <hasnaindar1234@gmail.com>
2025-09-19 16:25:35 +0500

    calculator

--- class5.py
+++ class5.py
@@ -2,12 +2,26 @@
 #     print("hello world")
 # greeting()
 
-#keyword arguments
-def sum(num1, num2):
-    print(num1 + num2)
-sum(num1= 1, num2= 2)    
+# keyword arguments
+# def sum(num1, num2):
+#     print(num1 + num2)
+# sum(num1= 1, num2= 4)    
 
 # positional arguments 
-def sum(num1, num2):
-    print(num1 + num2)
-sum(1, 2)    
+# def sum(num1, num2):
+#     print(num1 + num2)
+# sum(1, 2) 
+
+#return function
+# def catch(obj):
+#     print(f'Catched!{obj}')
+#     return("hasnain")
+# bag = catch('⚾')
+# print(bag)
+
+def catch(obj):
+    print(f'catched!{obj}')
+    return("hasnain")
+bag = catch('⚾')
+print(bag)
+   
